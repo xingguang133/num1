d4f1041b32fe9c573fa5db9c652dda4544369220
xingguang133 <2601549919@qq.com>
2023-09-07 16:16:13 +0800

    Added feature 1

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    return messages.getString("greeting") + ", " + name. touppercase ();
   }
 
   public static void main(String[] args) {
